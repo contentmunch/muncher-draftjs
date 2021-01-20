daea603c35f127f5d8a3bc2558b8c0e850a9f321
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2021-01-20 11:04:11 +0545

    feat: add simple editor

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {Muncher} from "./Muncher";
 export {MuncherSkeleton} from "./skeleton/MuncherSkeleton";
+export {MuncherSimple} from "./MuncherSimple";
+export {MuncherSimpleSkeleton} from "./skeleton/MuncherSimpleSkeleton";
